d9fc313a50c778b28eac2457ebb69f03d2222f5e
andrelake <andre.lake@hotmail.com>
2020-05-13 18:09:27 -0300

    Tests

--- src/test/java/com/andrelake/gubeetest/GubeeTestApplicationTests.java
+++ src/test/java/com/andrelake/gubeetest/GubeeTestApplicationTests.java
@@ -1,13 +1,86 @@
 package com.andrelake.gubeetest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import com.andrelake.gubeetest.domain.exception.ProductNotFoundException;
+import com.andrelake.gubeetest.domain.model.Product;
+import com.andrelake.gubeetest.domain.service.ProductService;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@TestPropertySource("/application-test.properties")
 class GubeeTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	private static final String NAME_PRODUCT_TEST = "Gubee BankApp";
 
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private ProductService service;
+	
+	@BeforeEach
+	public void setUp() {
+		
+		RestAssured.port = port;
+		RestAssured.basePath = "/gubeeproducts";
+	}
+	
+	@Test
+	public void shouldPersistProduct_WhenUseSave() {
+		
+		Product newProduct = createProduct();
+		service.save(newProduct);
+		
+		assertThat(newProduct.getId()).isNotNull();
+		assertThat(newProduct.getProductName()).isEqualTo(NAME_PRODUCT_TEST);
+	}
+	
+	@Test
+	public void shouldDeleteProduct_WhenUseDelete() {
+		
+		Product newProduct = createProduct();
+		service.save(newProduct);
+		
+		service.deleteById(newProduct.getId());
+		
+		assertThrows(ProductNotFoundException.class, () -> {
+			service.findById(newProduct.getId());
+		});
+	}
+	
+	@Test
+	public void shouldUpdateProductName_WhenUseSaveAfterChangeProperties() {
+		
+		Product newProduct = createProduct();
+		service.save(newProduct);
+		
+		newProduct.setProductName("Outro Gubee Bank");
+		service.save(newProduct);
+		
+		assertThat(newProduct.getProductName()).isEqualTo("Outro Gubee Bank");
+	}
+	
+	public Product createProduct() {
+		
+		String[] stack = new String[] {"Spring", "NodeJS", "Java 11"};
+		String[] targetMarket = new String[] {"Banco", "Banco Digital"};
+		
+		Product prod = new Product();
+		prod.setProductName(NAME_PRODUCT_TEST);
+		prod.setDescription("Ferramenta de gestão de bancos");
+		prod.setStack(stack);
+		prod.setTargetMarket(targetMarket);
+		
+		return prod;
+	}
 }
